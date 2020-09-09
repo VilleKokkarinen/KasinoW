82031fa77ccd7a0fa199e49a3081625d74a447a2
VilleKokkarinen <kokkarinen.ville@gmail.com>
2020-09-09 17:34:14 +0300

    commit

---
+++ src/constants/firebase.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCflEG1XfR5P-bC4V1Z__mv9fGiuv3sC08",
+  authDomain: "kasino-w.firebaseapp.com",
+  databaseURL: "https://kasino-w.firebaseio.com",
+  projectId: "kasino-w",
+  storageBucket: "kasino-w.appspot.com",
+  messagingSenderId: "980285000127",
+  appId: "1:980285000127:web:c7b3b350e37e4784b1f90a",
+  measurementId: "G-Q90D4M4DXL"
+};
